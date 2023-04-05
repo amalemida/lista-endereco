51b842e747644c3c2f563f8258078e3bbebd50bc
aMarceloAlmeida <amalemida@gmail.com>
2023-04-05 20:58:15 -0300

    ajuste do código

--- Programa.java
+++ Programa.java
@@ -3,8 +3,7 @@ import bd.daos.Fornecedores;
 public class Programa {
   public static void main(String[] args) {
     try {
-      Fornecedores.alterar(1, "13010120", 25, "SALA 1");
-      System.out.println("Fornecedor incluido com sucesso!");
+            System.out.println(Fornecedores.getFornecedor(1));
     } catch (Exception erro) {
       erro.printStackTrace();
       System.out.println(erro.getMessage());
